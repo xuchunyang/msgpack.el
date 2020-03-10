a8084b76a68d48aab671cbf53ea68eba2c2bb50a
Xu Chunyang <xuchunyang56@gmail.com>
2020-03-10 20:19:15 +0800

    Happy hacking

---
+++ msgpack.el
@@ -0,0 +1,33 @@
+;;; msgpack.el --- MessagePack Library               -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Xu Chunyang
+
+;; Author: Xu Chunyang
+;; Homepage: https://github.com/xuchunyang/msgpack.el
+;; Package-Requires: ((emacs "24.5"))
+;; Keywords: lisp
+;; Version: 0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Emacs Lisp library for MessagePack <https://msgpack.org/index.html>
+
+;;; Code:
+
+
+
+(provide 'msgpack)
+;;; msgpack.el ends here
